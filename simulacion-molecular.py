90c6fa3afcd96300366acc623999306b220d216e
LukaCarmona <lukacarmona115@gmail.com>
2024-09-25 15:54:22 +0200

    st.write en vez de print

--- simulacion-molecular.py
+++ simulacion-molecular.py
@@ -367,8 +367,8 @@ if st.session_state.mostrar:
                 distancias = st.session_state.resultado[0]
                 hartree_fall = st.session_state.resultado[1][1]
                 exacto = st.session_state.resultado[1][2]      
-                print("Resultado 0", st.session_state.resultado[0])               
-                print("Resultado 1", st.session_state.resultado[1])
+                st.write("Resultado 0", st.session_state.resultado[0])               
+                st.write("Resultado 1", st.session_state.resultado[1])
                 #llamo a la funcion que da nombre al x_label
                 x_label = x_label_content()
 
