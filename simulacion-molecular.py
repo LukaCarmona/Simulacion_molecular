da95c8c7af0d741cf0f1f064a78f3232b5e2d9e6
LukaCarmona <lukacarmona115@gmail.com>
2024-10-16 09:04:35 +0200

    cambio de () a []

--- simulacion-molecular.py
+++ simulacion-molecular.py
@@ -223,7 +223,7 @@ with st.sidebar:
             st.write(range_values)
             st.write(range_values(1))
             
-            if range_values(0) == range_values(1):
+            if range_values[0] == range_values[1]:
                 option = "Una sola distancia"
                 distancia_min = round(range_values(0), 1)
             # step = st.number_input("Seleccione el step para el gráfico", min_value=0.3, max_value=3.0, value=0.3, step=0.1)     
