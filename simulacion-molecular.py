15b3c959b50be2ca5b0105c075af3d9dc4629d96
LukaCarmona <lukacarmona115@gmail.com>
2024-10-18 09:48:03 +0200

    a

--- simulacion-molecular.py
+++ simulacion-molecular.py
@@ -112,12 +112,10 @@ with st.sidebar:
     if archived_type == "Simulación local":
         archived_type = 0
         st.session_state.archived_type = archived_type
-        st.rerun()
 
     elif archived_type == "Archivo":
         archived_type = 1
         st.session_state.archived_type = archived_type
-        st.rerun()
 
     # Cargar las opciones de moléculas desde el JSON     
     if st.session_state.archived_type == 0:
