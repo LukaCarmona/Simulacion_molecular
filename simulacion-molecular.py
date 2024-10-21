578c2a76b5e6ed85984c1922d049597b9bd62de3
LukaCarmona <lukacarmona115@gmail.com>
2024-10-21 16:44:19 +0200

    a

--- simulacion-molecular.py
+++ simulacion-molecular.py
@@ -254,16 +254,9 @@ with st.sidebar:
         col1, col2 = st.columns(2)
         with col1:
             aplicar_cambios()
-            if option != st.session_state.option:
-                st.write("option", option)
-                st.write("st.session_state.option", option)
-                st.session_state.mostrar = False  
+            
     else:
         aplicar_cambios()
-        if option != st.session_state.option:
-            st.write("option", option)
-            st.write("st.session_state.option", option)
-            st.session_state.mostrar = False  
 
 
     #si se ha pulsado el boton se crea el boton de descargar hamiltonianos
@@ -312,7 +305,7 @@ with st.sidebar:
                     st.write("No se ha podido crear el archivo")
 #--------------------------------------- CONTENIDO PRINCIPAL ---------------------------------------------        
 if st.session_state.mostrar:
-    
+    st.session_state.mostrar = False
     # col1, col2 = st.columns([1, 4])  # Ajusta el tamaño de las columnas según sea necesario
 
     # # Colocar el botón "HOME" en la primera columna
